f7e993b9adbd2b7bcbef91eee46eeb291a089e4c
Vit Listik <tivvit@seznam.cz>
2022-03-27 00:16:53 +0100

    fail on db row read

--- server/stycoon/stats.go
+++ server/stycoon/stats.go
@@ -2,7 +2,7 @@ package stycoon
 
 import (
 	"database/sql"
-	log "github.com/sirupsen/logrus"
+	"fmt"
 )
 
 type PlayerScore struct {
@@ -11,26 +11,23 @@ type PlayerScore struct {
 	Score  int64   `json:"score"`
 }
 
-func GetPlayerScores(db *sql.DB) []PlayerScore {
+func GetPlayerScores(db *sql.DB) ([]PlayerScore, error) {
 	var scores = make([]PlayerScore, 0)
 	rows, err := db.Query("select * from v_player_score;")
 	if err != nil {
-		log.Errorf("Query failed %v", err)
-		return scores
+		return scores, fmt.Errorf("query failed %v", err)
 	}
 
 	var score PlayerScore
 	for rows.Next() {
 		err = rows.Scan(&score.Player, &score.Price, &score.Score)
 		if err != nil {
-			log.Warningf("Row read failed %v", err)
-			continue
+			return scores, fmt.Errorf("row read failed %v", err)
 		}
 		scores = append(scores, score)
 	}
 	if err = rows.Err(); err != nil {
-		log.Warningf("Rows read failed %v", err)
-		return scores
+		return scores, fmt.Errorf("rows read failed: %v", err)
 	}
-	return scores
+	return scores, nil
 }
