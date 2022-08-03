b0caa14b2db7f2de36ba888351461f6621a2ee46
Vit Listik <tivvit@seznam.cz>
2022-08-04 00:15:49 +0200

    move models outside the dir

--- server-model-gen.sh
+++ server-model-gen.sh
@@ -2,16 +2,14 @@
 
 rm -rf server/gen
 
+rm -f server/stycoon/helpers.go
+rm -f server/stycoon/impl.go
+rm -f server/stycoon/model_*.go
+
 docker-compose run server-gen
 
-mkdir -p server/stycoon/models
-
-mv server/gen/go/*.go server/stycoon/models/
+mv server/gen/go/helpers.go server/stycoon/
+mv server/gen/go/impl.go server/stycoon/
+mv server/gen/go/model_*.go server/stycoon/
 
 rm -rf server/gen
-
-# Garbage
-rm server/stycoon/models/api.go
-rm server/stycoon/models/routers.go
-rm server/stycoon/models/logger.go
-rm server/stycoon/models/error.go
