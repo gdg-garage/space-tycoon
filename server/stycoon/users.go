778ac4da13547728c4ac50bba98e99cc19f86915
Vit Listik <tivvit@seznam.cz>
2022-08-04 01:38:22 +0200

    consistent naming of player

--- server/stycoon/users.go
+++ server/stycoon/users.go
@@ -34,9 +34,9 @@ func LoggedUser(req *http.Request, sessionManager sessions.Store) (Credentials, 
 	} else {
 		return Credentials{}, errors.New("username is missing = user is not logged")
 	}
-	if username, ok := session.Values["player"]; ok {
-		if username, ok = username.(string); ok {
-			user.Player = username.(string)
+	if player, ok := session.Values["player"]; ok {
+		if player, ok = player.(string); ok {
+			user.Player = player.(string)
 		} else {
 			// TODO - how to handle
 			log.Warn().Msg("Player is corrupted")
