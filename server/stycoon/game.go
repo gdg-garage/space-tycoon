8ba179629de06c81940cacbd80b76f83c98e1e40
Vit Listik <tivvit@seznam.cz>
2022-08-10 01:21:32 +0200

    use t_report_player_score for player net worth

--- server/stycoon/game.go
+++ server/stycoon/game.go
@@ -228,25 +228,33 @@ func (game *Game) getCommodityAmounts() (map[int]map[string]*TradingResource, er
 
 func (game *Game) setPlayers() error {
 	var players = make(map[string]PlayersValue)
-	rows, err := game.db.Query("select `id`, `name`, `color`, `money` from t_player")
+	rows, err := game.db.Query("select `id`, `name`, `color`, t_player.`money`, score.`commodities`, score.`ships`, score.`total` from t_player left join t_report_player_score score on t_player.id = score.player where score.tick  = ?", game.Tick.Tick-1)
 	if err != nil {
 		return fmt.Errorf("query failed %v", err)
 	}
 	var id int
 	var player PlayersValue
-	var color string
+	var color sql.NullString
+	var commodities, ships, total sql.NullInt64
 	for rows.Next() {
-		err = rows.Scan(&id, &player.Name, &color, &player.NetWorth.Money)
+		err = rows.Scan(&id, &player.Name, &color, &player.NetWorth.Money, &commodities, &ships, &total)
 		if err != nil {
 			return fmt.Errorf("row read failed %v", err)
 		}
-		err = json.Unmarshal([]byte(color), &player.Color)
-		if err != nil {
-			return err
+		if color.Valid {
+			err = json.Unmarshal([]byte(color.String), &player.Color)
+			if err != nil {
+				return err
+			}
 		}
-		err = game.setPlayerNetWorth(id, &player.NetWorth)
-		if err != nil {
-			return err
+		if commodities.Valid {
+			player.NetWorth.Resources = commodities.Int64
+		}
+		if ships.Valid {
+			player.NetWorth.Ships = ships.Int64
+		}
+		if total.Valid {
+			player.NetWorth.Total = total.Int64
 		}
 		players[strconv.Itoa(id)] = player
 	}
@@ -254,21 +262,5 @@ func (game *Game) setPlayers() error {
 		return fmt.Errorf("rows read failed: %v", err)
 	}
 	game.players = players
-	return nil
-}
-
-func (game *Game) setPlayerNetWorth(playerId int, playerNetWorth *NetWorth) error {
-	err := game.db.QueryRow("select `price` from v_player_ships_worth where player = ?", playerId).Scan(&playerNetWorth.Ships)
-	if err != nil {
-		return err
-	}
-	err = game.db.QueryRow("select `price` from v_player_commodities_worth where player = ?", playerId).Scan(&playerNetWorth.Resources)
-	if err != nil {
-		return err
-	}
-	err = game.db.QueryRow("select `price` from v_player_total_worth where player = ?", playerId).Scan(&playerNetWorth.Total)
-	if err != nil {
-		return err
-	}
 	return nil
 }
