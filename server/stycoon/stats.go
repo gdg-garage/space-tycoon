a6c02a288a29ef37f968280f237e476b174ba81e
Tomas Maly <malytomas@ucpu.cz>
2022-08-10 02:33:14 +0200

    static data (alternative kind-of squashed stuff)

---
+++ server/stycoon/stats.go
@@ -0,0 +1,31 @@
+package stycoon
+
+import (
+	"fmt"
+)
+
+type PlayerScore struct {
+	Player int32 `json:"player"`
+	Price  int64 `json:"price"`
+	Score  int64 `json:"score"`
+}
+
+func (game *Game) GetPlayerScores() ([]PlayerScore, error) {
+	var scores = make([]PlayerScore, 0)
+	rows, err := game.db.Query("select `player`, `price`, `score` from v_player_score")
+	if err != nil {
+		return scores, fmt.Errorf("query failed %v", err)
+	}
+	var score PlayerScore
+	for rows.Next() {
+		err = rows.Scan(&score.Player, &score.Price, &score.Score)
+		if err != nil {
+			return scores, fmt.Errorf("row read failed %v", err)
+		}
+		scores = append(scores, score)
+	}
+	if err = rows.Err(); err != nil {
+		return scores, fmt.Errorf("rows read failed: %v", err)
+	}
+	return scores, nil
+}
