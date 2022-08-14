f4c0b6fb59c1a110c0451f9383c1efdfd8ebf770
Eva Mayerova <evca.mayerova@gmail.com>
2022-08-14 14:00:48 +0200

    update sql query

--- server/database/query.go
+++ server/database/query.go
@@ -6,14 +6,10 @@ import (
 )
 
 func CheckObjectIsPlanet(db *sql.DB, id int64) (bool, error) {
-	rows, err := db.Query("select * from t_object join t_planet on t_planet.id = t_object.id where t_object.id = ?", id)
+	var cnt int
+	err := db.QueryRow("select count(*) from t_object join t_planet on t_planet.id = t_object.id where t_object.id = ?", id).Scan(&cnt)
 	if err != nil {
 		return false, fmt.Errorf("query failed %v", err)
 	}
-	for rows.Next() {
-		var owner int64
-		rows.Scan(&owner)
-		return true, nil
-	}
-	return false, nil
+	return cnt == 1, nil
 }
