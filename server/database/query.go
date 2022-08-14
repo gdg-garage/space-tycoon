f6eb80c9be7f8905e5923ca35fcf8d96393af776
Eva Mayerova <evca.mayerova@gmail.com>
2022-08-14 14:16:22 +0200

    fetch planet IDs in ProcessCommands

--- server/database/query.go
+++ server/database/query.go
@@ -13,3 +13,20 @@ func CheckObjectIsPlanet(db *sql.DB, id int64) (bool, error) {
 	}
 	return cnt == 1, nil
 }
+
+func GetPlanetIDs(db *sql.DB) (map[int64]struct{}, error) {
+	planetIDs := map[int64]struct{}{}
+	rows, err := db.Query("select `id` from t_object join t_planet on t_planet.id = t_object.id")
+	var id int64
+	for rows.Next() {
+		err = rows.Scan(&id)
+		if err != nil {
+			return planetIDs, fmt.Errorf("row read failed %v", err)
+		}
+		planetIDs[id] = struct{}{}
+	}
+	if err = rows.Err(); err != nil {
+		return planetIDs, fmt.Errorf("rows read failed: %v", err)
+	}
+	return planetIDs, nil
+}
--- server/stycoon/command_trade.go
+++ server/stycoon/command_trade.go
@@ -15,24 +15,18 @@ func NewTradeCommand(command Command) *TradeCommand {
 	}
 }
 
-func (game *Game) processTrade(id int64, playerOwnedShips map[int64]struct{}, command Command) error {
+func (game *Game) processTrade(id int64, playerOwnedShips map[int64]struct{}, planetIDs map[int64]struct{}, command Command) error {
 	c := NewTradeCommand(command)
 	err := AssertTradeCommandRequired(*c)
 	if err != nil {
 		return err
 	}
 
-	_, shipOwnedByPlayer := playerOwnedShips[int64(id)]
-	if shipOwnedByPlayer {
+	_, shipOwnedByPlayer := playerOwnedShips[id]
+	_, targetIsPlanet := planetIDs[id]
+
+	if shipOwnedByPlayer || targetIsPlanet {
 		return database.ReplaceTradeCommand(game.db, id, command.Type, *c.Target, *c.Resource, *c.Amount)
 	}
-
-	targetIsPlanet, err := database.CheckObjectIsPlanet(game.db, id)
-	if err != nil {
-		return err
-	}
-	if !targetIsPlanet {
-		return fmt.Errorf("trading with another players ship is forbidden")
-	}
-	return database.ReplaceTradeCommand(game.db, id, command.Type, *c.Target, *c.Resource, *c.Amount)
+	return fmt.Errorf("trading with another players ship is forbidden")
 }
