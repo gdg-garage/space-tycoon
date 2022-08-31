157ccd03ad403b202afed2422f0f26564ecfd722
Tomas Maly <malytomas@ucpu.cz>
2022-08-31 03:34:47 +0200

    visualization: update bundle.js; add web-gen.sh which regenerates bundle.js

---
+++ web-gen.sh
@@ -0,0 +1,12 @@
+
+docker-compose up visualization-gen
+cd web/js/client
+npm install
+npm link
+cd ../..
+npm link $PWD/js/client/
+npm run build
+
+echo
+echo
+echo "run 'cd web && npm run watch' to automatically detected changes in index.js and regenerate bundle.js"
